7c84ff74a84779083b7ffabdd5cc3110786f106c
MecLoving <shawn.loder@gmail.com>
2022-09-01 14:34:32 -0400

    pushing changes

--- pages/what.js
+++ pages/what.js
@@ -4,6 +4,8 @@ export default function What() {
             Cette endroit sur internet sert a expulser mes idées de ma tête pour permettre de vivre plus relax. La psychiatre l'as dit: "les idées c'est de l'énergie, arrête de penser"
 
             Depuis l'accident j'essai de faire ce que j'aime et encourage les autres a en faire de même. Le bonheur es grandement sous-estimé.
+
+            Faire que la société arrête de juger en tout temps, c'est néfaste pour le progrès. 
         </div>
     )
 }
